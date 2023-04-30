a3c398ba280036c9936e07370e760f4853cdb670
Alexandra Valkova <alexandra.valkova.97@gmail.com>
2023-04-30 12:08:14 +0200

    Updated Index :speech_balloon:

--- PatientsRegistry/Migrations/Configuration.cs
+++ PatientsRegistry/Migrations/Configuration.cs
@@ -16,21 +16,12 @@ namespace PatientsRegistry.Migrations
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            //  to avoid creating duplicate seed data.
 
             context.Users.AddOrUpdate(
                   u => u.ID,
                   new User { ID = 1, Username = "alexandra", Password = "password", FirstName = "Alexandra", LastName = "Valkova", IsDoctor = false },
-                  new User { ID = 2, Username = "yosifova", Password = "password", FirstName = "Diana", LastName = "Yosifova", IsDoctor = true }
-                );
+                  new User { ID = 2, Username = "yosifova", Password = "password", FirstName = "Diana", LastName = "Yosifova", IsDoctor = true });
         }
     }
 }
